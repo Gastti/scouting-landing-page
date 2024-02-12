3d6b53ae06262ae7888b80526456b34a6770c8fd
Gastti <gutigas96@gmail.com>
2024-02-12 17:17:56 -0300

    test

--- app/api/twitter/route.ts
+++ app/api/twitter/route.ts
@@ -3,12 +3,14 @@ import { NextResponse } from "next/server";
 const token = 'AAAAAAAAAAAAAAAAAAAAAJUOsQEAAAAAUEcyAkFT35n3YSVmv5DhDnatMzo%3Dko1rwRE9u1I1CqwvTTalJoMuyTKQ84aMuyY4UvCW3wBPWtJ0f8'
 
 export async function GET(){
-    const tweets = await fetch('https://api.twitter.com/2/tweets?ids=Scouting_CyGD', {
+    let tweets;
+    const response = await fetch('https://api.twitter.com/2/tweets?ids=Scouting_CyGD', {
         headers: {
             "Authorization": `Bearer ${token}`
         }
-    })
-    console.log(tweets)
+    }).then(response => response.json())
+    .then(data => tweets = data)
+
     return NextResponse.json({
         route: 'Twitter',
         tweets
