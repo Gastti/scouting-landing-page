261db2ed00458e406ec074cbdb99d8da336d5e6f
Gastti <gutigas96@gmail.com>
2024-02-15 14:17:46 -0300

    fix height of the navbar menu

--- app/ui/navigation/navbar.responsive.tsx
+++ app/ui/navigation/navbar.responsive.tsx
@@ -40,7 +40,7 @@ export default function NavbarResponsive() {
                 classNames="my-node"
                 unmountOnExit
             >
-                <div ref={nodeRef} className={`my-node flex w-full h-[calc(100dvh-80px)] top-0 left-0 py-5 justify-between flex-col`} onClick={handleOpened}>
+                <div ref={nodeRef} className={`my-node flex w-full h-[calc(100dvh-70px)] top-0 left-0 py-5 justify-between flex-col`} onClick={handleOpened}>
                     <div className='flex flex-col gap-5 items-start'>
                         <NavLinks className='text-[1rem] grow-0' />
                     </div>
