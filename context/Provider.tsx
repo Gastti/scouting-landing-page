053ab945d778671873e345e89cf6e680b1e7e669
Gastti <gutigas96@gmail.com>
2024-02-15 12:31:12 -0300

    backup

--- context/Provider.tsx
+++ context/Provider.tsx
@@ -5,14 +5,12 @@ import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
 const Providers = ({ children }: { children: React.ReactNode }) => {
     return (
         <>
-            <div className='z-30 absolute'>
-                <ProgressBar
-                    height="4px"
-                    color="#13cc3b"
-                    options={{ showSpinner: false }}
-                    shallowRouting
-                />
-            </div>
+            <ProgressBar
+                height="4px"
+                color="#13cc3b"
+                options={{ showSpinner: false }}
+                shallowRouting
+            />
             {children}
         </>
     );
